6a9df41ca775bdc5ef97dbf17cdba222b04e8dbc
Maia <gbmmaia.contato@gmail.com>
2023-09-15 15:33:32 -0300

    feat (auth_presentation) Criado possíveis estados de Auth_Bloc

--- lib/features/auth/presentation/auth_bloc.dart
+++ lib/features/auth/presentation/auth_bloc.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gaze/core/enums/update_user.dart';
+import 'package:gaze/features/auth/domain/models/user_model.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
--- lib/features/auth/presentation/auth_state.dart
+++ lib/features/auth/presentation/auth_state.dart
@@ -2,9 +2,35 @@ part of 'auth_bloc.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
+
+  @override
+  List<Object> get props => [];
 }
 
 class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+class AuthLoading extends AuthState {}
+
+class SignedIn extends AuthState {
+  const SignedIn(this.user);
+
+  final UserModel user;
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [user];
+}
+
+class SignedUp extends AuthState {
+  const SignedUp();
+}
+
+class UserUpdated extends AuthState {
+  const UserUpdated(this.message);
+
+  final String message;
+
+  @override
+  List<String> get props => [message];
 }
