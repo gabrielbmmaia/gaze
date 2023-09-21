ac9639cf4bea2c14cec69f4b9beda4c9b6082c60
Maia <gbmmaia.contato@gmail.com>
2023-09-21 18:17:38 -0300

    feat (series_domain) Criação de SeriesRepo com função getPopularSeries

---
+++ lib/features/series/domain/repositories/series_repo.dart
@@ -0,0 +1,11 @@
+
+import 'package:gaze/core/utils/typedefs.dart';
+import 'package:gaze/features/series/domain/models/series_model.dart';
+
+abstract class SeriesRepo {
+  const SeriesRepo();
+
+  ResultFuture<List<SeriesModel>> getPopularSeries();
+
+
+}
