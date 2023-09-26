a620e6cfdc309e323bbce8981730e8a17680415e
Maia <gbmmaia.contato@gmail.com>
2023-09-26 19:00:28 -0300

    feat (series_domain) Criado get_netflix_series.dart e testes de quando a chamada é um sucesso

---
+++ lib/features/series/domain/usecases/get_netflix_series.dart
@@ -0,0 +1,14 @@
+
+import 'package:gaze/core/usecases/usecase.dart';
+import 'package:gaze/core/utils/typedefs.dart';
+import 'package:gaze/features/series/domain/models/series_model.dart';
+import 'package:gaze/features/series/domain/repositories/series_repo.dart';
+
+class GetNetflixSeriesUseCase extends UseCaseWithoutParams<List<SeriesModel>> {
+  const GetNetflixSeriesUseCase(this._repo);
+
+  final SeriesRepo _repo;
+
+  @override
+  ResultFuture<List<SeriesModel>> call() => _repo.getNetflixSeries();
+}
