abc5ed26b313de4d3b08e1e3859b6d9f168fd749
Maia <gbmmaia.contato@gmail.com>
2023-10-03 20:56:44 -0300

    feat (series_domain) Criação de get_series_by_gender.dart com teste de sucesso

---
+++ lib/features/series/domain/usecases/get_series_by_gender.dart
@@ -0,0 +1,15 @@
+import 'package:gaze/core/usecases/usecase.dart';
+import 'package:gaze/core/utils/typedefs.dart';
+import 'package:gaze/features/series/domain/models/series_model.dart';
+import 'package:gaze/features/series/domain/repositories/series_repo.dart';
+
+class GetSeriesByGenderUseCase
+    extends UseCaseWithParams<List<SeriesModel>, String> {
+  const GetSeriesByGenderUseCase(this._repo);
+
+  final SeriesRepo _repo;
+
+  @override
+  ResultFuture<List<SeriesModel>> call(String params) =>
+      _repo.getSeriesByGenre(params);
+}
